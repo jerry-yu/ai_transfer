898f16be81d9ad91430f14f9a7f4aad372ffb05f
jerry-yu <anhuitianyu@163.com>
2023-04-21 16:57:43 +0800

    add go code for sending tokens

---
+++ go_send/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+
+	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/config"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/signature"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+)
+
+func main() {
+	from := os.Args[1]
+	to := os.Args[2]
+	amount := os.Args[3]
+
+	embending_account := []string{"Alice", "Bob", "Dave"}
+
+	var from_address, to_address signature.KeyringPair
+	var err error
+	for _, v := range embending_account {
+		if v == from {
+			from_address, err = signature.KeyringPairFromSecret("//"+v, 42)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else if v == to {
+			to_address, err = signature.KeyringPairFromSecret("//"+v, 42)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	log.Println(from_address, to_address)
+
+	api, err := gsrpc.NewSubstrateAPI(config.Default().RPCURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	meta, err := api.RPC.State.GetMetadataLatest()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bal, ok := new(big.Int).SetString(amount, 10)
+	if !ok {
+		log.Fatal(fmt.Errorf("failed to convert balance"))
+	}
+
+	to_multi_address, err := types.NewMultiAddressFromAccountID(to_address.PublicKey)
+	if err != nil {
+		panic(err)
+	}
+	c, err := types.NewCall(meta, "Balances.transfer", to_multi_address, types.NewUCompact(bal))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the extrinsic
+	ext := types.NewExtrinsic(c)
+
+	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rv, err := api.RPC.State.GetRuntimeVersionLatest()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := types.CreateStorageKey(meta, "System", "Account", from_address.PublicKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var accountInfo types.AccountInfo
+	ok, err = api.RPC.State.GetStorageLatest(key, &accountInfo)
+	if err != nil || !ok {
+		log.Fatal(err)
+	}
+
+	nonce := uint32(accountInfo.Nonce)
+	o := types.SignatureOptions{
+		BlockHash:          genesisHash,
+		Era:                types.ExtrinsicEra{IsMortalEra: false},
+		GenesisHash:        genesisHash,
+		Nonce:              types.NewUCompactFromUInt(uint64(nonce)),
+		SpecVersion:        rv.SpecVersion,
+		Tip:                types.NewUCompactFromUInt(100),
+		TransactionVersion: rv.TransactionVersion,
+	}
+
+	// Sign the transaction using Alice's default account
+	err = ext.Sign(from_address, o)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Send the extrinsic
+	hash, err := api.RPC.Author.SubmitExtrinsic(ext)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Balance transferred hash ", hash.Hex())
+
+}
